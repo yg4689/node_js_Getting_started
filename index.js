efe63302ef02a0edde32dd0802b9f9435721271f
yg4689 <pjohn4@horizon.csueastbay.edu>
2019-10-01 10:51:08 -0700

    master

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var mongodb = require('mongodb');
+var path = require('path');
+var router = express.Router();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res, next) {
+  res.render('pages/index', { title: 'Express' });
+});
+
+app.get('/mongodb', function(req, res, next) {
+  mongodb.MongoClient.connect('mongodb://johnpreetha4:LockTon@312@ds117866.mlab.com:17866/heroku_ck1rc6j8', function(err, client) {
+    if(err) throw err;
+      var db = client.db('heroku_ck1rc6j8');
+      var Routes = db.collection('Routes');
+      useNewUrlParser: true
+      Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
+      if(err) throw err;
+        res.render('pages/mongodb', {results: docs});
+      });
+      client.close(function (err) {
+        if(err) throw err;
+      });
+    });//end of connect
+});//end app.get
+
+app.listen(PORT, function() {
+  console.log(`Listening on Port ${PORT}`);
+});
